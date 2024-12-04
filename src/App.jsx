12be175c20818e53d2d59a46d614f71aaf959c27
Ricardo Giaviti <rgiaviti@gmail.com>
2024-12-04 01:11:14 +0000

    Seção 3 - Lição 39

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@
 // For this project to work on CodeSandbox, image assets ("assets") folder
 // must be stored in the public folder (as it's the case by default in this project)
 
+function Header() {
+  return (
+    <header>
+      <img src="assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
